9682627b3bdd430d5937868c7c1bad5737d8aab5
Surendra Vanteddu <surendra.vanteddu@gmail.com>
2022-04-23 16:51:29 -0500

    update user api to return user info

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,11 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send(req.headers.user)
+  res.send({
+    firstname: 'Ron',
+    lastname: 'J',
+    email: 'ron.j@miviewis.com'
+  })
 })
 
 export default router
